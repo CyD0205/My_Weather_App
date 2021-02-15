7ac7009887e011f2d9b806f218278ac02bee6ccb
CyD0205 <78613064+CyD0205@users.noreply.github.com>
2021-02-15 19:38:27 +0100

    Adding current date and month

--- src/script.js
+++ src/script.js
@@ -1,9 +1,12 @@
 let now = new Date();
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let day = days[now.getDay()];
+let date = (now.getDate() < 10 ? '0' : '') + (now.getDate());
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let month = months[now.getMonth()];
 let hours = (now.getHours() < 10 ? '0' : '') + (now.getHours());
 let minutes = (now.getMinutes() < 10 ? '0' : '') + (now.getMinutes());
-document.querySelector("#today-content").innerHTML = `${day}, ${hours}:${minutes}`;
+document.querySelector("#today-content").innerHTML = `${day} ${date} ${month}, at ${hours}:${minutes}`;
 
 function searchCity(event) {
   event.preventDefault(); 
